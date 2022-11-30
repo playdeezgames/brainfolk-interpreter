45ce81f24913a82b226501636fb8adad0060a262
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-11-30 04:45:19 -0600

    fellowship has a name

--- src/QFTS.Business/World.vb
+++ src/QFTS.Business/World.vb
@@ -1,2 +1,17 @@
 Public Class World
+    Private _worldData As WorldData
+    Sub New()
+        _worldData = New WorldData
+        Dim playerFellowshipId = Guid.NewGuid
+        _worldData.PlayerFellowshipId = playerFellowshipId
+        _worldData.Fellowships.Add(playerFellowshipId, New FellowshipData With {.Name = "Yer Company"})
+    End Sub
+    Public ReadOnly Property PlayerFellowship As Fellowship
+        Get
+            If _worldData.PlayerFellowshipId.HasValue Then
+                Return New Fellowship(_worldData, _worldData.PlayerFellowshipId.Value)
+            End If
+            Return Nothing
+        End Get
+    End Property
 End Class
--- src/QuestForTheStarjool/PlayProcessor.vb
+++ src/QuestForTheStarjool/PlayProcessor.vb
@@ -2,6 +2,7 @@
     Friend Sub Run(world As World)
         Do
             AnsiConsole.Clear()
+            AnsiConsole.MarkupLine($"Fellowship: {world.PlayerFellowship.Name}")
             Dim prompt As New SelectionPrompt(Of String) With {.Title = "[olive]Now What?[/]"}
             prompt.AddChoice(AbandonGameText)
             Select Case AnsiConsole.Prompt(prompt)
