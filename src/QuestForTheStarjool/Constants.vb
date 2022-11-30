42f97c18cc4300ce7976dd70382f08e9894e1b32
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-11-30 04:10:58 -0600

    main menu with quit

---
+++ src/QuestForTheStarjool/Constants.vb
@@ -0,0 +1,5 @@
+﻿Module Constants
+    Friend Const GameTitle = "Quest for the Starjool!!"
+    Friend Const OkText = "Ok"
+    Friend Const QuitText = "Quit"
+End Module
---
+++ src/QuestForTheStarjool/Utility.vb
@@ -0,0 +1,7 @@
+﻿Module Utility
+    Friend Sub OkPrompt()
+        Dim prompt As New SelectionPrompt(Of String) With {.Title = String.Empty}
+        prompt.AddChoice(OkText)
+        AnsiConsole.Prompt(prompt)
+    End Sub
+End Module
